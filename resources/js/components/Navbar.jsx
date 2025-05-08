1d8c0e356ea6ffb32f51077e8f23ee18bc7ea99d
unknown <rakunnaka@gmail.com>
2025-05-08 13:59:08 +0700

    feath:update

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center p-5 h-[70px]  rounded-sm bg-slate-200">
+        <nav className="flex justify-between items-center p-5 h-[80px]  rounded-sm bg-slate-200">
             <h1 className="font-semibold text-xl">NyamNyamGo!</h1>
             <Link to="/login" >
-                <button className="p-2 w-16 rounded-2xl bg-slate-700 text-white">
+                <button className="p-2 w-20  rounded-2xl bg-slate-700 text-white">
                     Login
                 </button>
             </Link>
